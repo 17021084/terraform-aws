141f1b799bf5afd1c867e21652389c1244d1dde0
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2022-11-16 01:57:03 +0900

    terraform ref and apply it to create NGINX SERVER

--- EC2_SG_NGINX/main.tf
+++ EC2_SG_NGINX/main.tf
@@ -14,16 +14,16 @@ provider "aws" {
 
 
 
-resource "aws_security_group" "instance_file" {
+resource "aws_security_group" "instance_firewall" {
   name        = "allow_ALL_TCP_80_SSH_TERRAFROM"
   description = "Allow all tcp 80 and ssh , terrafrom demo"
   # default vpc
   # vpc_id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    description      = "all ssh access"
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = var.input_ipv4
   }
@@ -36,5 +36,36 @@ resource "aws_security_group" "instance_file" {
     cidr_blocks      = var.input_ipv4
   }
 
+  // allow all
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
 
+resource "aws_instance" "app_server" {
+  # free tier, check region and ami install 
+  ami = "ami-0de5311b2a443fb89"
+
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance have been created by terraform"
+  }
+  user_data = <<EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo amazon-linux-extras install nginx1 -y 
+    sudo systemctl enable nginx
+    sudo systemctl start nginx
+  EOF
+
+
+# name not id
+  security_groups = [aws_security_group.instance_firewall.name]   # ref
+
+}
